1696601a594f6ca6d11d94725c67966181ad6c9e
Gabriel Cicotoste <gabriel@example.com>
2026-08-11 15:28:16 -0300

    fix(alembic): encadeia migration 008 apos a 007 (US-08)

--- alembic/versions/008_add_codigo_acesso_aula_sincrona.py
+++ alembic/versions/008_add_codigo_acesso_aula_sincrona.py
@@ -1,7 +1,7 @@
 """add codigo_acesso and data_hora_fim to aulas_sincronas
 
 Revision ID: 008
-Revises: 006_add_cascade_delete_curso
+Revises: 007
 Create Date: 2026-08-10
 """
 
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 from alembic import op
 
 revision: str = "008"
-down_revision: Union[str, None] = "006_add_cascade_delete_curso"
+down_revision: Union[str, None] = "007"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
